54b35be2f079b7d4a3250c3a20102ea5d749e923
Emilio Molinari <xates97@gmail.com>
2020-09-02 20:43:54 +0200

    General refactoring and added multiple memes

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from telegram.ext import *
 
 APP_URL = os.getenv("APP_URL")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-PORT = int(os.getenv("PORT"))
+PORT = int(os.getenv("PORT") or 8443)
 
 
 class CustomHandler(SimpleHTTPRequestHandler):
@@ -21,78 +21,120 @@ class CustomHandler(SimpleHTTPRequestHandler):
         self.send_error(HTTPStatus.NOT_FOUND, "File not found")
 
 
-def make_meme(top_text: str, bottom_text: str, template: str, target: str) -> None:
-    font_size = 72
-    outline_size = 15
-
-    img = Image.open(template)
+def make_meme(texts: list, offsets: list, template: str, target: str) -> None:
+    img = Image.open(f"{template}.jpg")
     width = img.size[0]
     height = img.size[1]
 
-    # find biggest font size that works
-    font = ImageFont.truetype("impact.ttf", font_size)
-    top_text_size = font.getsize(top_text)
-    bottom_text_size = font.getsize(bottom_text)
-
-    while top_text_size[0] > width - 20 or bottom_text_size[0] > width - 20:
-        font_size = font_size - 1
-        font = ImageFont.truetype("impact.ttf", font_size)
-        top_text_size = font.getsize(top_text)
-        bottom_text_size = font.getsize(bottom_text)
-
-    # find top centered position for top text
-    top_text_position_x = (width/2) - (top_text_size[0]/2)
-    top_text_position_y = 0
-    top_text_position = (top_text_position_x, top_text_position_y)
-
-    # find bottom centered position for bottom text
-    bottom_text_position_X = (width/2) - (bottom_text_size[0]/2)
-    bottom_text_position_Y = height - bottom_text_size[1] - outline_size
-    bottom_text_position = (bottom_text_position_X, bottom_text_position_Y)
-
     draw = ImageDraw.Draw(img)
+    outline_size = 15
 
-    # draw outlines
-    # there may be a better way
-    outline_range = int(font_size / outline_size)
-    for x in range(-outline_range, outline_range + 1):
-        for y in range(-outline_range, outline_range + 1):
-            draw.text((top_text_position[0] + x, top_text_position[1] + y), top_text, (0, 0, 0), font=font)
-            draw.text((bottom_text_position[0] + x, bottom_text_position[1] + y), bottom_text, (0, 0, 0), font=font)
+    for text, offset in zip(texts, offsets):
+        # find biggest font size that fits
+        font_size = 72
+        font = ImageFont.truetype("impact.ttf", font_size)
+        text_size = font.getsize(text)
 
-    draw.text(top_text_position, top_text, (255, 255, 255), font=font)
-    draw.text(bottom_text_position, bottom_text, (255, 255, 255), font=font)
+        while text_size[0] > width - 20:
+            font_size = font_size - 1
+            font = ImageFont.truetype("impact.ttf", font_size)
+            text_size = font.getsize(text)
 
-    img.save(f"www/i/{target}.jpg")
+        # compute position for text
+        text_position_x = (width / 2) - (text_size[0] / 2)
+        text_position_y = offset if offset >= 0 else height - text_size[1] - outline_size
+        text_position = (text_position_x, text_position_y)
+
+        # draw outlines
+        # there might be a better way
+        outline_range = int(font_size / outline_size)
+        for x in range(-outline_range, outline_range + 1):
+            for y in range(-outline_range, outline_range + 1):
+                draw.text((text_position[0] + x, text_position[1] + y), text, (0, 0, 0), font=font)
+
+        # draw text
+        draw.text(text_position, text, (255, 255, 255), font=font)
+
+    img.save(f"www/{target}.jpg")
 
     thumb_size = (200, 200)
     img.thumbnail(thumb_size)
-    img.save(f"www/i/{target}-thumb.jpg")
+    img.save(f"www/{target}-thumb.jpg")
 
     return width, height
 
 
 def start(update: Update, context: CallbackContext) -> None:
     uuid = str(uuid4())
-    make_meme("THIS IS AN", "INLINE BOT!!1!!1", "seagull.jpg", uuid)
-    update.message.reply_photo(f"{APP_URL}i/{uuid}.jpg")
+    make_meme(["THIS IS AN", "INLINE BOT!!1!!1"], [0, -1], "small", uuid)
+    update.message.reply_photo(f"{APP_URL}{uuid}.jpg")
 
 
 def inlinequery(update: Update, context: CallbackContext) -> None:
-    top_text, _, bottom_text = update.inline_query.query.upper().partition("\n")
-    uuid = str(uuid4())
-    width, height = make_meme(top_text if bottom_text else "",
-                              bottom_text.replace("\n", " ") or top_text,
-                              "seagull.jpg", uuid)
+    texts = update.inline_query.query.upper().split("\n")
 
-    results = [
-        InlineQueryResultPhoto(
-            id=uuid,
-            photo_url=f"{APP_URL}i/{uuid}.jpg",
-            thumb_url=f"{APP_URL}i/{uuid}-thumb.jpg",
-            photo_width=width,
-            photo_height=height
-        )]
+    n = len(texts)
+    results = []
+
+    if n == 1:
+        uuid = str(uuid4())
+        width, height = make_meme(texts, [-1], "small", uuid)
+        results.append(
+            InlineQueryResultPhoto(
+                id=uuid,
+                photo_url=f"{APP_URL}{uuid}.jpg",
+                thumb_url=f"{APP_URL}{uuid}-thumb.jpg",
+                photo_width=width,
+                photo_height=height
+            ))
+
+    if n == 2:
+        uuid = str(uuid4())
+        width, height = make_meme(texts, [0, -1], "small", uuid)
+        results.append(
+            InlineQueryResultPhoto(
+                id=uuid,
+                photo_url=f"{APP_URL}{uuid}.jpg",
+                thumb_url=f"{APP_URL}{uuid}-thumb.jpg",
+                photo_width=width,
+                photo_height=height
+            ))
+
+    if n == 2:
+        uuid = str(uuid4())
+        width, height = make_meme([texts[0], "*INHALES*", texts[1]], [0, 665, -1], "medium", uuid)
+        results.append(
+            InlineQueryResultPhoto(
+                id=uuid,
+                photo_url=f"{APP_URL}{uuid}.jpg",
+                thumb_url=f"{APP_URL}{uuid}-thumb.jpg",
+                photo_width=width,
+                photo_height=height
+            ))
+
+    if n == 3:
+        uuid = str(uuid4())
+        width, height = make_meme(texts, [0, 665, -1], "medium", uuid)
+        results.append(
+            InlineQueryResultPhoto(
+                id=uuid,
+                photo_url=f"{APP_URL}{uuid}.jpg",
+                thumb_url=f"{APP_URL}{uuid}-thumb.jpg",
+                photo_width=width,
+                photo_height=height
+            ))
+
+    if n == 4:
+        uuid = str(uuid4())
+        width, height = make_meme(texts, [0, 677, 1342, -1], "large", uuid)
+        results.append(
+            InlineQueryResultPhoto(
+                id=uuid,
+                photo_url=f"{APP_URL}{uuid}.jpg",
+                thumb_url=f"{APP_URL}{uuid}-thumb.jpg",
+                photo_width=width,
+                photo_height=height
+            ))
 
     update.inline_query.answer(results)
 
@@ -104,7 +146,7 @@ def main() -> None:
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(InlineQueryHandler(inlinequery))
 
-    os.makedirs("www/i", exist_ok=True)
+    os.makedirs("www", exist_ok=True)
     httpd = HTTPServer(("", PORT), CustomHandler)
 
     updater.bot.delete_webhook()
